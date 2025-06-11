5c70604c7e899ec1d06f5c56190fa8613601e0f3
Lorenzo Furrer <lorenzo.furrer@pearson.com>
2025-06-11 20:42:26 +0100

    gitignore python venv. Basic upload file

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, render_template
 import subprocess
 import json
+import os
 
 app = Flask(__name__)
 
 EXIF_TOOLS_PATH = 'C:\Tools\ExifViewers\exiftool-13.30_64\exiftool.exe'
+
+TEMP_DIR = os.path.join(os.path.dirname(__file__), 'temp')
+if not os.path.exists(TEMP_DIR):
+    os.makedirs(TEMP_DIR)
 
 def is_json(content):
     try:
@@ -23,22 +28,26 @@ def format_json(content):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        image_path = request.form['image_path']
-        try:
-            output = subprocess.check_output([EXIF_TOOLS_PATH, image_path]).decode('utf-8')
-            print(output)
-            exif_data = {}
-            for line in output.split('\n'):
-                if ':' in line:
-                    key, value = line.split(':', 1)
-                    key = key.strip()
-                    value = value.strip()
-                    if is_json(value):
-                        value = format_json(value)
-                    exif_data[key] = value
-            return render_template('exif.html', exif_data=exif_data)
-        except Exception as e:
-            return str(e)
+        if 'image_file' in request.files:
+            image_file = request.files['image_file']
+            file_path = os.path.join(TEMP_DIR, image_file.filename)
+            image_file.save(file_path)
+            try:
+                output = subprocess.check_output([EXIF_TOOLS_PATH, file_path]).decode('utf-8')
+                exif_data = {}
+                for line in output.split('\n'):
+                    if ':' in line:
+                        key, value = line.split(':', 1)
+                        key = key.strip()
+                        value = value.strip()
+                        if is_json(value):
+                            value = format_json(value)
+                        exif_data[key] = value
+                return render_template('exif.html', exif_data=exif_data)
+            except Exception as e:
+                return str(e)
+        else:
+            return "No file uploaded."
     return render_template('index.html')
 
 if __name__ == '__main__':
